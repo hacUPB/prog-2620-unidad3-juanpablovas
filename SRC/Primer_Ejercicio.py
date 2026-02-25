c4f2564bacc8d30c3d31274e388333f3ea796c69
Juan Pablo Vásquez Muñoz <juanp.vasquezm@upb.edu.co>
2026-02-25 15:01:55 -0500

    Primer commit de prueba

---
+++ SRC/Primer_Ejercicio.py
@@ -0,0 +1,14 @@
+a = 34
+b = 45
+
+#A continuación creo un condicional
+
+if a > b:
+    print("A es mayor que B")
+else:
+    print("A es menor que B")
+
+# Un bucle que imprime 10 veces la palabra UPB
+
+for i in range(10):
+    print("UPB")
